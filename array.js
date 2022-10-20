f037dd32b08604fd0a91a8bdb127aaecb8ea412f
trevorfitz0 <trevorfitz0@gmail.com>
2022-10-20 13:02:22 -0600

    Add array calls

--- array.js
+++ array.js
@@ -1,3 +1,10 @@
 var strings = ["Trevor", "Fitz", "Turing", "School"];
 var nums = [1, 2, 3, 4,];
 var bools = [true, false, true, false];
+
+//This will call for Trevor
+moreStrings = strings[0];
+//This will call for 4
+moreNums = nums[3];
+//This will call for false
+moreBools = bools[1];
